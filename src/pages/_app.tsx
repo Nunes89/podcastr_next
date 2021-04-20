d216c352b2523d021782da4fb1eeb9d9d461f3f1
Nunes <GilsonNunes_dev@outlook.com>
2021-04-20 13:13:42 -0300

    Adding basic layout of the app components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import Header from "../components/Header";
 import "../styles/global.scss";
+import Header from "../components/Header";
+import Player from "../components/Player/index";
+import styles from "../styles/app.module.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
     </>
   );
 }
